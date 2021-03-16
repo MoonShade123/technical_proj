a10de6de6c9c2f3d8283420283049374cf032e3b
MoonShade123 <sosnovscky.nikita@yandex.ru>
2021-03-16 16:40:53 +0300

    Roles

--- src/main/java/com/example/technical_proj/repository/RoleRepository.java
+++ src/main/java/com/example/technical_proj/repository/RoleRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
     Role findRoleByName(final String name);
 }
 
