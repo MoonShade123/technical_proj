2a7e3549361bfb0f7cd54482dad68347976e3ff4
MoonShade123 <sosnovscky.nikita@yandex.ru>
2021-03-16 16:28:48 +0300

    Roles

---
+++ src/main/java/com/example/technical_proj/repository/PrivilegeRepository.java
@@ -0,0 +1,10 @@
+package com.example.technical_proj.repository;
+
+import com.example.technical_proj.model.Privilege;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PrivilegeRepository extends JpaRepository<Privilege, Long> {
+    Privilege findByName(String name);
+}
---
+++ src/main/java/com/example/technical_proj/security/SetupDataLoader.java
@@ -0,0 +1,76 @@
+package com.example.technical_proj.security;
+
+import com.example.technical_proj.model.Privilege;
+import com.example.technical_proj.model.Role;
+import com.example.technical_proj.model.User;
+import com.example.technical_proj.repository.PrivilegeRepository;
+import com.example.technical_proj.repository.RoleRepository;
+import com.example.technical_proj.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class SetupDataLoader implements
+        ApplicationListener<ContextRefreshedEvent> {
+
+    boolean alreadySetup = false;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PrivilegeRepository privilegeRepository;
+
+    @Override
+    @Transactional
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+
+        if (alreadySetup)
+            return;
+        Privilege readPrivilege
+                = createPrivilegeIfNotFound("READ_PRIVILEGE");
+        Privilege writePrivilege
+                = createPrivilegeIfNotFound("WRITE_PRIVILEGE");
+
+        List<Privilege> adminPrivileges = Arrays.asList(
+                readPrivilege, writePrivilege);
+        createRoleIfNotFound("ROLE_ADMIN", adminPrivileges);
+        createRoleIfNotFound("ROLE_USER", Collections.singletonList(readPrivilege));
+
+        alreadySetup = true;
+    }
+
+    @Transactional
+    Privilege createPrivilegeIfNotFound(String name) {
+
+        Privilege privilege = privilegeRepository.findByName(name);
+        if (privilege == null) {
+            privilege = new Privilege(name);
+            privilegeRepository.save(privilege);
+        }
+        return privilege;
+    }
+
+    @Transactional
+    Role createRoleIfNotFound(
+            String name, Collection<Privilege> privileges) {
+
+        Role role = roleRepository.findRoleByName(name);
+        if (role == null) {
+            role = new Role(name);
+            role.setPrivileges(privileges);
+            roleRepository.save(role);
+        }
+        return role;
+    }
+}
